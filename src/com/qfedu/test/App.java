7dd44ff8fa824bfe4e123dfdca7613a65e7ce00a
userchenxinx <51064792+userchenxinx@users.noreply.github.com>
2019-05-28 11:43:14 +0800

    add info

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hhh");
         System.out.println("hahaha");
+        System.out.println("hahaha");
     }
 }
