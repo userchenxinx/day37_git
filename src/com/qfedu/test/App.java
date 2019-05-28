cf8fe81634cc1de81468293998099fdb67316314
admin <2225913091@qq.com>
2019-05-28 11:50:16 +0800

    sss

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -3,7 +3,7 @@ package com.qfedu.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("hhh");
-        System.out.println("hahaha");
-        System.out.println("hahaha");
+        System.out.println("haa");
+        int a = 1;
     }
 }
